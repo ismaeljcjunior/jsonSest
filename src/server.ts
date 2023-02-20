39a1e8cbe7fba5c346ed6c30dd67ea1f8004c86c
Ismael Junior <ismaeljcjunior@gmail.com>
2023-02-20 14:23:54 -0300

    feat initial

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from 'express'
+
+
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+
+interface ApiResponse {
+    json: any;
+  }
+
+app.post('/apiresponse', async (req: Request, res: Response) => {
+    const data = req.body
+    console.log(data)
+
+    res.send({ data });
+});
+
+
+app.get('/', async (req: Request, res: Response) => {
+    res.send('apiresponse');
+});
+
+app.listen(4001, () => {
+    console.log('Server started on port 4001');
+});
